9b16ffb4dd8f5e5617eb54eefcc63591a98792c7
Thomas E <slashdevsda@gmail.com>
2019-03-27 21:40:17 +0200

    add/correct types annotations

--- conftest.py
+++ conftest.py
@@ -1,8 +1,9 @@
 import pytest
+import aiohttp
 from restaurants.app import init_app
 
 @pytest.fixture
-async def client(aiohttp_client):
+async def client(aiohttp_client: aiohttp.Client) -> None:
     '''
     The fixture for the initialize client.
     '''
--- restaurants/app.py
+++ restaurants/app.py
@@ -1,13 +1,38 @@
+from textwrap import wrap
 from aiohttp import web
-from typing import Optional, List
+import aiosqlite
+from typing import Optional, List, AsyncGenerator
 
+async def database_connect(app: web.Application) -> AsyncGenerator[None, None]:
+    '''
+    This coroutine is responsible for database initialization.
+
+    As we use an in-memory sqlite (for the sake of simplicity),
+    initialisation and data-insertion is done right here
+    '''
+    db = await aiosqlite.connect(":memory:")
+    app['db'] = db
+    await db.execute(wrap('''
+      CREATE DATABASE test(name VARCHAR);
+    '''))
+    yield 
+    await db.close()
+    
+    
+
+async def test_handler(request: web.Request) -> web.Response:
+    return web.Response(status=200)
+    
 def init_app(config: Optional[List[str]] = None) -> web.Application:
     app = web.Application()
 
+
+    
     # init context here
     # - configuration
     # - routes
     # - database connexions
     # - enventual template engines
+    app.router.add_route('GET', '/test', test_handler)
     app.cleanup_ctx.extend([])
     return app
