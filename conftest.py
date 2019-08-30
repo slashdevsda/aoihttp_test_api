7262feb8e2cc53140aee962286e3247943455fc5
Thomas E <slashdevsda@gmail.com>
2019-08-30 16:03:18 +0200

    Remove a useless comment & rewording

--- conftest.py
+++ conftest.py
@@ -4,11 +4,8 @@ from restaurants.app import init_app
 @pytest.fixture
 async def client(aiohttp_client): # type: ignore
     '''
-    The fixture for the initialize client.
+    HTTP client fixture
     '''
     app = init_app([])
 
     return await aiohttp_client(app)
-
-
-#Callable[[Coroutine[Request, None, Response]], Coroutine[Any, Any, Response]]", expected "Coroutine[Request, None, Response
